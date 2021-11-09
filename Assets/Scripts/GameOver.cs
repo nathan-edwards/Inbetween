0b4692a7a0bcb0d5d22f5eeeb0d9760e2a32510b
jzhang282 <jieyingzhang02@gmail.com>
2021-11-09 02:31:44 +0000

    added trees in biome 1

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public void displayGameOver(){
+		gameObject.SetActive(true);
+	}
+}
