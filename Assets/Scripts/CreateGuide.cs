90db00dfa871bb0dd6c135082563f6cb2a31869f
Nathan Edwards <nathane1726@gmail.com>
2021-11-11 20:00:46 +0000

    Removed Thirdparty Testing Assets and Misc Changes

--- Assets/Scripts/CreateGuide.cs
+++ Assets/Scripts/CreateGuide.cs
@@ -10,5 +10,6 @@ public class CreateGuide : MonoBehaviour
     {
         // Instantiate at position (0, 0, 0) and zero rotation.
         Instantiate(particles, GameObject.FindGameObjectsWithTag("Player")[0].transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 }
