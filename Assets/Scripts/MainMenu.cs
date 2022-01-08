33655780aedefe8a7540099f0b44fedc6c24ae91
Melisa Bogdan <melisa.bogdan@gmail.com>
2022-01-09 00:54:33 +0200

    Menu Scene fully functional - needs design

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
